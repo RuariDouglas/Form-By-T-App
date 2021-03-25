26b8e8d1e592d2973339181b7876c0204ff8dd4f
RuariDouglas <70585404+RuariDouglas@users.noreply.github.com>
2021-03-25 17:55:46 +0100

    .

--- src/DummyProducts.js
+++ src/DummyProducts.js
@@ -1,41 +1,48 @@
 import ProductImage from "./images/hero.jpeg";
+import { v4 as uuidv4 } from "uuid";
 
 const dummyProducts = [
   {
     title: "Sky Blue Dress",
     price: "$75.00",
     imgUrl: ProductImage,
-    productId: "#",
+    productId: uuidv4(),
   },
   {
     title: "Red Dress",
     price: "$115.00",
     imgUrl: ProductImage,
+    productId: uuidv4(),
   },
   {
     title: "Black Dress",
     price: "$275.00",
     imgUrl: ProductImage,
+    productId: uuidv4(),
   },
   {
     title: "Green Hat",
     price: "$15.00",
     imgUrl: ProductImage,
+    productId: uuidv4(),
   },
   {
     title: "Scrunchie",
     price: "$25.00",
     imgUrl: ProductImage,
+    productId: uuidv4(),
   },
   {
     title: "Mask",
     price: "$15.00",
     imgUrl: ProductImage,
+    productId: uuidv4(),
   },
   {
     title: "Big Bag",
     price: "$105.00",
     imgUrl: ProductImage,
+    productId: uuidv4(),
   },
 ];
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,13 @@ import React from "react";
 
 // COMPONENTS
 import Hero from "../components/home/Hero";
+import ProductGallery from "../components/ProductGallery";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <ProductGallery show={6} />
     </>
   );
 };
