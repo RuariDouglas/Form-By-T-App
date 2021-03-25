2bf42111e97bad714a02b25038fddbce4eb08d52
RuariDouglas <70585404+RuariDouglas@users.noreply.github.com>
2021-03-25 17:54:02 +0100

    Set up dummy products and gallery

---
+++ src/DummyProducts.js
@@ -0,0 +1,42 @@
+import ProductImage from "./images/hero.jpeg";
+
+const dummyProducts = [
+  {
+    title: "Sky Blue Dress",
+    price: "$75.00",
+    imgUrl: ProductImage,
+    productId: "#",
+  },
+  {
+    title: "Red Dress",
+    price: "$115.00",
+    imgUrl: ProductImage,
+  },
+  {
+    title: "Black Dress",
+    price: "$275.00",
+    imgUrl: ProductImage,
+  },
+  {
+    title: "Green Hat",
+    price: "$15.00",
+    imgUrl: ProductImage,
+  },
+  {
+    title: "Scrunchie",
+    price: "$25.00",
+    imgUrl: ProductImage,
+  },
+  {
+    title: "Mask",
+    price: "$15.00",
+    imgUrl: ProductImage,
+  },
+  {
+    title: "Big Bag",
+    price: "$105.00",
+    imgUrl: ProductImage,
+  },
+];
+
+export default dummyProducts;
---
+++ src/components/ProductGallery.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// PRODUCTS
+import dummyProducts from "../DummyProducts";
+
+const ProductGallery = (props) => {
+  const show = props.show;
+  return (
+    <div className="p-gallery">
+      <div>I am the propduct Gallery</div>
+    </div>
+  );
+};
+
+export default ProductGallery;
