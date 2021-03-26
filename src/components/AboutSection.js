a7ae352b55e2ff0c1023555fdf2df9f7832ba790
RuariDouglas <70585404+RuariDouglas@users.noreply.github.com>
2021-03-26 13:03:42 +0100

    About section styling

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -6,14 +6,18 @@ import FormByT from "../images/formbyt.svg";
 const AboutSection = () => {
   return (
     <section className="about-section">
-      <img src={FormByT} alt="Form By T" />
       <p>
         Form By T is a conscious brand upholding sustainable values at every
-        point of the process whilst balancing attention to detail and quality.
+        point of the process whilst balancing attention to detail and quality.{" "}
+        <br /> <br />
         The result is a collection that has been designed and produced locally
-        with a considered trans-seasonal focus. Every piece is made to order
-        with care by Tessa in her Melbourne home studio.
+        with a considered trans-seasonal focus.
+        <br />
+        <br />
+        Every piece is made to order with care by Tessa in her Melbourne home
+        studio.
       </p>
+      <img src={FormByT} alt="Form By T" />
     </section>
   );
 };
